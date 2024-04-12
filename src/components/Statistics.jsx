027732cbcdf0851ff4b3c2811a3c3d915042867a
Norbi <norbertocarlos30@gmail.com>
2024-04-12 19:50:57 +0200

    statisztik-change

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -27,15 +27,15 @@ export default function Statistics({ views }) {
     return (
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead className="text-xs text-center text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 text-center">
                             TELEPÜLÉS
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 text-center">
                             LÁTVÁNYOSSÁGOK
                         </th>
-                        <th scope="col" className="px-6 py-3">
+                        <th scope="col" className="px-6 py-3 text-center">
                             ÁTLAG ÁR
                         </th>
                     </tr>
@@ -43,13 +43,13 @@ export default function Statistics({ views }) {
                 <tbody>
                     {Object.entries(places).map(([key, value]) => (
                         <tr key={key} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
                                 {key}
                             </th>
-                            <td className="px-6 py-4">
+                            <td className="px-6 py-4 text-center">
                                 {value.numOfAllSettlemets}
                             </td>
-                            <td className="px-6 py-4">
+                            <td className="px-6 py-4 text-center">
                                 {value.fullPrice / value.numOfAllSettlemets}
                             </td>
                         </tr>
@@ -58,10 +58,10 @@ export default function Statistics({ views }) {
                         <th scope="col" className="px-6 py-3">
                             ÖSSZEGZÉS
                         </th>
-                        <td className="px-6 py-4">
+                        <td className="px-6 py-4 text-center">
                             {numOfViews && numOfViews}
                         </td>
-                        <td className="px-6 py-4">
+                        <td className="px-6 py-4 text-center">
                             {numOfViews && Math.round(avgPrice / numOfViews)}
                         </td>
                     </tr>
